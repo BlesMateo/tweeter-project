fe2d2156970fe05f795caf4b89951664008a96a0
BlesMateo <blesildaangelicamateo@gmail.com>
2021-11-16 19:17:18 -0500

    Added a character counter that changes the counter to red when character exceeds limit

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  $("#tweet-text").on('keyup', function(event) {
+    let charLength = $(this).val().length
+    let charLimit = 140;
+    let charLeft = charLimit - charLength;
+
+    console.log(charLeft)
+    $(".counter").text(charLeft);
+
+    if(charLeft < 0) {
+      $(".counter").css("color", "red");
+    } else {
+      $(".counter").css("color", "white");
+    }
+
+  });
+});
