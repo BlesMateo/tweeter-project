ac49cf1bf593b9d4aa16d37acbb788ea429918ae
BlesMateo <blesildaangelicamateo@gmail.com>
2021-11-17 20:19:37 -0500

    Added a loadtweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,30 +5,30 @@
  */
 
 //Data derived from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
 
 //Test or driver code (temporary). Eventually will get this from the server.
 const tweetData = {
@@ -50,6 +50,13 @@ function renderTweets(tweets) {
     $('#tweet-container').append($tweet);
   }
 }
+
+const loadTweets = function() {
+  $.ajax("/tweets", {method: "GET" }).then(function (tweets) {
+    renderTweets(tweets);
+  });
+};
+loadTweets();
 
 //Function takes in a tweet object returns a tweet <article> element containing the entire HTML structure of the tweet.
 function createTweetElement (tweetData) {
@@ -76,7 +83,6 @@ function createTweetElement (tweetData) {
 
 
 $(document).ready(function() {
-  renderTweets(data);
   $("#post-tweet").submit(function(event) {
     event.preventDefault();
     console.log("New Tweet!");
@@ -89,5 +95,7 @@ $(document).ready(function() {
     })
 
   });
+
+
 });
  // $("#tweet-container").prepend($tweet);
