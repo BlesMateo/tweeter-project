52202cd0e8123d80be4827f84ede41188dbea465
BlesMateo <blesildaangelicamateo@gmail.com>
2021-11-18 21:32:27 -0500

    eslinted the entire file to check for issues and errors

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,27 +9,27 @@
 $(document).ready(function() {
 
 
-//Function takes in an array of tweet objects and then appending each one to the #tweets-container
-function renderTweets(tweets) {
-  $('#tweet-container').empty()
-  for (let tweet of tweets) {
-    const $tweet = createTweetElement(tweet);
-    $('#tweet-container').prepend($tweet);
-  }
-}
+  //Function takes in an array of tweet objects and then appending each one to the #tweets-container
+  const renderTweets = (tweets) => {
+    $('#tweet-container').empty();
+    for (let tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweet-container').prepend($tweet);
+    }
+  };
 
 
-//Function to escape text and use it inside .html
-const escape = function (str) {
-  let div = document.createElement("div");
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-};
+  //Function to escape text and use it inside .html
+  const escape = function(str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
-//Function takes in a tweet object returns a tweet <article> element containing the entire HTML structure of the tweet.
-function createTweetElement (tweetData) {
-  const {user, content, created_at} = tweetData;
-  const tweetElementHTML = $(`<article class="tweet">
+  //Function takes in a tweet object returns a tweet <article> element containing the entire HTML structure of the tweet.
+  const createTweetElement = (tweetData) => {
+    const {user, content, created_at} = tweetData;
+    const tweetElementHTML = $(`<article class="tweet">
   <header>
     <span class="username"><img src="${user.avatars}">${user.name}</span>
     <span class="userHandle">${user.handle}</span>
@@ -45,45 +45,45 @@ function createTweetElement (tweetData) {
         <i class="fas fa-heart"></i>
       </div>
     </footer>
-</article>`)
-  return tweetElementHTML;
-};
+</article>`);
+    return tweetElementHTML;
+  };
 
 
-//Function that uses JQuery to make a request to /tweets and receive the tweets as JSON
-const loadTweets = function() {
-  $.ajax("/tweets", {method: "GET" }).then(function (tweets) {
-    renderTweets(tweets);
-  });
-};
+  //Function that uses JQuery to make a request to /tweets and receive the tweets as JSON
+  const loadTweets = function() {
+    $.ajax("/tweets", {method: "GET" }).then(function (tweets) {
+      renderTweets(tweets);
+    });
+  };
 
-//Displaying form validations using JQuery if there is no or excessive form input
-const $form = $('form');
-$form.on('submit', function(event) {
-  event.preventDefault()
-  $("#error-notification").slideUp();
-  const value = $(this).find("#tweet-text").val();
+  //Displaying form validations using JQuery if there is no or excessive form input
+  const $form = $('form');
+  $form.on('submit', function(event) {
+    event.preventDefault();
+    $("#error-notification").slideUp();
+    const value = $(this).find("#tweet-text").val();
     if (!value.trim()) {
       $("#error-notification")
-      .html("⚠ There must be something on your mind! ⚠").slideDown();
-     }
+        .html("⚠ There must be something on your mind! ⚠").slideDown();
+    }
     if (value.length > 140) {
       $("#error-notification")
-      .html("⚠ Woah there! Take a breather and let's try this again! ⚠").slideDown();
+        .html("⚠ Woah there! Take a breather and let's try this again! ⚠").slideDown();
     } else {
 
-    $.ajax("/tweets", {
-    method:"post",
-    data: $("#post-tweet").serialize(),
-    success: () => {
-    $("#tweet-text").val('');
-    $(".counter").val(140);
-    loadTweets()}
-
-  })
-}
-});
-loadTweets();
+      $.ajax("/tweets", {
+        method:"post",
+        data: $("#post-tweet").serialize(),
+        success: () => {
+          $("#tweet-text").val('');
+          $(".counter").val(140);
+          loadTweets();
+        }
+      });
+    }
+  });
+  loadTweets();
 });
 
 
